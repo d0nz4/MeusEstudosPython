a9cb7e69d4fee1e73ef97208dfa9fe1346ea4191
Anderson B da silva <and0000z3@gmail.com>
2023-01-07 13:39:53 -0300

    Add files via upload

---
+++ armazenamento.py
@@ -0,0 +1,40 @@
+# declaração das variaveis de programa
+# A, B, C, D, são declaradas na atribuição de valores
+
+#atribui um valor 0 a variavel D.
+D = 0 
+
+# exibe na tela o valor de D com frase explicativa.
+print("O valor de D: ", D)
+
+#atribui um valor 2 a variavel A.
+A = 2 
+
+# exibe na tela o valor de D com frase explicativa.
+print("O valor de A: ", A)
+
+#atribui um valor 3 a variavel B.
+B = 3 
+
+# exibe na tela o valor de D com frase explicativa.
+print("O valor de B: ", B)
+
+#atribui um valor 5 a variavel C.
+C = 5 
+
+# exibe na tela o valor de D com frase explicativa.
+print("O valor de C: ", C)
+
+# Atualize a variavel C o seu valor anterior acrescido de 1
+C = C + 1
+
+# exibe na tela o valor de C (atualizado) com frase explicativa
+print("O valor de C: ", C)
+
+# atribui á variavel D a soma do valor de A com o valor atual de C
+D = A + C
+
+# exibe na tela o valor de D (atualizado) com frase explicativa
+print("Valor de D:", D)         
+
+
